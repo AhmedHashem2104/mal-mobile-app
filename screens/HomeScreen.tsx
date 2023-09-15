9666a503e5e7e7c97037018742fabdaa0e28434b
ahmedhashem <ahmed.hashem@bld.ai>
2023-09-16 01:42:10 +0300

    v1.1

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,14 +1,28 @@
 import { View, Text, StyleSheet, TouchableOpacity, Image } from "react-native";
 import React from "react";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }: any) => {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
+        <Image
+          style={styles.headerBackground}
+          source={require("../assets/EllipseHeader.png")}
+        />
         <View style={styles.headerRow}>
           <Text style={styles.headerText}>Hello, Aya!</Text>
           <TouchableOpacity>
-            <Text style={styles.headerText}>Logout</Text>
+            <Text
+              style={styles.headerText}
+              onPress={() =>
+                navigation.reset({
+                  index: 0, // The index of the screen to navigate to (0 is the first screen)
+                  routes: [{ name: "Register" }], // The new stack of screens to reset to
+                })
+              }
+            >
+              Logout
+            </Text>
           </TouchableOpacity>
         </View>
         <View style={styles.balance}>
@@ -16,6 +30,26 @@ const HomeScreen = () => {
           <TouchableOpacity>
             <Image source={require("../assets/info-circle.png")} />
           </TouchableOpacity>
+        </View>
+        <View style={styles.amount}>
+          <Text style={styles.headerTextAmount}>SAR</Text>
+          <Text style={styles.headerAmount}>100</Text>
+        </View>
+      </View>
+      <View style={styles.invoiceContainer}>
+        <View style={styles.leftInvoice}>
+          <View style={styles.invoiceText}>
+            <Text style={styles.invoiceText}>
+              Scan invoice QR code and get your
+            </Text>
+            <Text style={styles.invoiceCachback}>CASHBACK</Text>
+          </View>
+          <TouchableOpacity style={styles.invoiceButton}>
+            <Text style={styles.invoiceButtonText}>Know More</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.rightInvoice}>
+          <Image source={require("../assets/invoiceImg.png")} />
         </View>
       </View>
     </View>
@@ -54,6 +88,78 @@ const styles = StyleSheet.create({
     alignItems: `center`,
     marginTop: 30,
     gap: 10,
+  },
+  amount: {
+    flexDirection: `row`,
+    justifyContent: `center`,
+    alignItems: `center`,
+    marginTop: 10,
+    gap: 9,
+  },
+  headerTextAmount: {
+    fontSize: 12,
+    fontWeight: "400",
+    color: `white`,
+  },
+  headerAmount: {
+    fontSize: 24,
+    fontWeight: "500",
+    color: `white`,
+  },
+  invoiceContainer: {
+    flexDirection: `row`,
+    alignItems: `center`,
+    justifyContent: `space-between`,
+    backgroundColor: `white`,
+    borderRadius: 15,
+    marginLeft: `auto`,
+    marginRight: `auto`,
+    width: `90%`,
+    marginTop: -50,
+    paddingVertical: 25,
+    paddingHorizontal: 12,
+  },
+  leftInvoice: {
+    paddingLeft: 25,
+    width: `60%`,
+  },
+  rightInvoice: {
+    width: `40%`,
+  },
+  invoiceText: {
+    flexDirection: `column`,
+    fontWeight: "600",
+    lineHeight: 25,
+    fontSize: 18,
+    fontStyle: `normal`,
+    color: `#0F0F0F`,
+  },
+  invoiceCachback: {
+    fontWeight: "800",
+    lineHeight: 25,
+    fontSize: 18,
+    fontStyle: `normal`,
+    textTransform: `uppercase`,
+    color: `#0F0F0F`,
+  },
+  invoiceButton: {
+    width: `70%`,
+    height: 40,
+    justifyContent: `center`,
+    alignItems: `center`,
+    borderRadius: 12,
+    backgroundColor: `#003`,
+    marginTop: 10,
+  },
+  invoiceButtonText: {
+    color: `white`,
+    fontWeight: "400",
+  },
+  headerBackground: {
+    position: `absolute`,
+    width: `60%`,
+    height: `100%`,
+    right: 0,
   },
 });
 
